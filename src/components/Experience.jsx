e9d172c7b2f8f81df564cfd0b2d292fdc80bc40e
Mohammad Ali Yazdani <mohammadaliyazdani21@gmail.com>
2025-02-24 11:18:20 +0300

    refactor: group floor with other models and reposition it

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -27,14 +27,6 @@ export const Experience = () => {
 
       {/* BACKGROUND */}
 
-      <RigidBody colliders={false} type="fixed" name="void">
-        <mesh position={[0, -1.5, 0]} rotation={[-Math.PI / 2, 0, 0]}>
-          <planeGeometry args={[90, 90]} />
-          <meshBasicMaterial color="#e3daf7" toneMapped={false} />
-        </mesh>
-        <CuboidCollider position={[0, -3.5, 0]} args={[50, 0.1, 50]} sensor />
-      </RigidBody>
-
       {currentKana && (
         <Text position={[0, -1, -20]}>
           {currentKana.name.toUpperCase()}
@@ -44,6 +36,15 @@ export const Experience = () => {
 
       <group position-y={-1}>
         <Kicker />
+        {/* FLOOR */}
+        <RigidBody colliders={false} type="fixed" name="void">
+          <mesh position={[0, -0.9, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+            <planeGeometry args={[90, 90]} />
+            <meshBasicMaterial color="#e3daf7" toneMapped={false} />
+          </mesh>
+          <CuboidCollider position={[0, -3.5, 0]} args={[50, 0.1, 50]} sensor />
+        </RigidBody>
+
         {/* STAGE */}
         <Stage position-y={-0.92} />
         <RigidBody
