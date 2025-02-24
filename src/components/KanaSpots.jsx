555e9f4d216944f3bf52f60d41327f6480bfa9a1
Mohammad Ali Yazdani <mohammadaliyazdani21@gmail.com>
2025-02-24 15:31:13 +0300

    feat: improve kanas ui

--- src/components/KanaSpots.jsx
+++ src/components/KanaSpots.jsx
@@ -1,12 +1,35 @@
-import { Center, Cylinder, Text3D } from "@react-three/drei";
+import { Center, Cylinder, Sphere, Text3D } from "@react-three/drei";
 import { CylinderCollider, RigidBody } from "@react-three/rapier";
 import { useGameStore } from "../store";
+import { useControls } from "leva";
 
 export const KanaSpots = () => {
   const level = useGameStore((state) => state.level);
   const currentStage = useGameStore((state) => state.currentStage);
   const mode = useGameStore((state) => state.mode);
   const kanaTouched = useGameStore((state) => state.kanaTouched);
+
+  const config = useControls({
+    meshPhysicalMaterial: false,
+    transmissionSampler: false,
+    backside: false,
+    samples: { value: 10, min: 1, max: 32, step: 1 },
+    resolution: { value: 1024, min: 256, max: 2048, step: 256 },
+    transmission: { value: 1, min: 0, max: 1 },
+    roughness: { value: 0.0, min: 0, max: 1, step: 0.01 },
+    thickness: { value: 0, min: 0, max: 10, step: 0.01 },
+    ior: { value: 1, min: 1, max: 5, step: 0.01 },
+    chromaticAberration: { value: 0, min: 0, max: 1 },
+    anisotropy: { value: 0, min: 0, max: 1, step: 0.01 },
+    distortion: { value: 0.0, min: 0, max: 1, step: 0.01 },
+    distortionScale: { value: 0, min: 0.01, max: 1, step: 0.01 },
+    temporalDistortion: { value: 0, min: 0, max: 1, step: 0.01 },
+    clearcoat: { value: 1, min: 0, max: 1 },
+    attenuationDistance: { value: 1, min: 0, max: 10, step: 0.01 },
+    attenuationColor: "#ffffff",
+    color: "#efbeff",
+    bg: "#ffffff",
+  });
 
   if (!level) {
     return null;
@@ -31,6 +54,10 @@ export const KanaSpots = () => {
           </Cylinder>
         </RigidBody>
 
+        <Sphere scale={[1.22, 1.22, 1.22]} position={[0, 0.8, 0]}>
+          <meshPhysicalMaterial {...config} />
+        </Sphere>
+
         <Center position-y={0.8}>
           <Text3D
             font={"./fonts/Noto Sans JP ExtraBold_Regular.json"}
@@ -40,7 +67,7 @@ export const KanaSpots = () => {
             {mode === "hiragana"
               ? kana.character.hiragana
               : kana.character.katakana}
-            <meshNormalMaterial />
+            <meshStandardMaterial color="#ebbe89" toneMapped={false} />
           </Text3D>
         </Center>
       </group>
